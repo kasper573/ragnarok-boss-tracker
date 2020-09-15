5ad1000ea5ea2afd5ac1f3a7ac0f817d4c4afc0f
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-15 21:01:29 +0200

    set kill time to now when initializing hunts

--- src/createHunt.ts
+++ src/createHunt.ts
@@ -3,5 +3,5 @@ import { Boss } from "./Boss";
 export const createHunt = (boss: Boss) => ({
   boss,
   map: boss.map,
-  killTime: new Date(Date.now() - boss.spawnTime * 1000),
+  killTime: new Date(),
 });
