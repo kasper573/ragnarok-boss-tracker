da287e791fab348d8b25d81ea26b9d15b2911489
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-17 00:17:11 +0200

    Correct invalid import

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { App } from "./App";
-import { bosses } from "../fixtures/bossesFixture";
+import { bosses } from "../fixtures/bosses";
 
 test("renders without runtime errors", () => {
   expect(() => render(<App bosses={bosses} />)).not.toThrow();
