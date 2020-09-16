a019e201d63f46c44b6dbddf6fce1396c8b15d8d
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-16 13:06:36 +0200

    Update HuntTimelineItem layout to fit better into compact devices

---
+++ src/HuntInfo.tsx
@@ -0,0 +1,54 @@
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import { Hunt } from "./Hunt";
+import { Tooltip, useMediaQuery, useTheme } from "@material-ui/core";
+
+export type HuntInfoProps = { hunt: Hunt };
+
+export const HuntInfo: React.FC<HuntInfoProps> = ({
+  hunt: {
+    map,
+    boss: { name, spawnCooldown, spawnWindow },
+  },
+}) => {
+  const theme = useTheme();
+  const compact = useMediaQuery(theme.breakpoints.down("xs"));
+  return (
+    <>
+      {compact ? (
+        <Tooltip title={name}>
+          <Typography noWrap variant="h6" component="h1">
+            {abbreviate(name)}
+          </Typography>
+        </Tooltip>
+      ) : (
+        <Typography noWrap variant="h6" component="h1">
+          {name}
+        </Typography>
+      )}
+      <Typography noWrap>
+        {compact ? "" : "Cooldown: "}
+        {spawnCooldown}~{spawnCooldown + spawnWindow}m
+      </Typography>
+      <Typography noWrap>
+        {compact ? "" : "Map: "}
+        {map}
+      </Typography>
+    </>
+  );
+};
+
+function abbreviate(str: string) {
+  const words = str.split(/\s+/);
+  switch (words.length) {
+    case 1:
+      return str;
+    case 2:
+      return `${words[0]} ${words[1][0].toUpperCase()}`;
+    default:
+      return words
+        .map((word) => word[0])
+        .join("")
+        .toUpperCase();
+  }
+}
--- src/HuntTimelineItem.tsx
+++ src/HuntTimelineItem.tsx
@@ -5,7 +5,6 @@ import TimelineDot from "@material-ui/lab/TimelineDot";
 import FastfoodIcon from "@material-ui/icons/Fastfood";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
-import Typography from "@material-ui/core/Typography";
 import MuiTimelineItem from "@material-ui/lab/TimelineItem";
 import { SpawnTime } from "./SpawnTime";
 import styled from "styled-components";
@@ -13,8 +12,7 @@ import Paper from "@material-ui/core/Paper";
 import { Hunt } from "./Hunt";
 import { IconButton, Tooltip } from "@material-ui/core";
 import { Delete, Edit } from "@material-ui/icons";
-import { Row } from "./Row";
-import { Column } from "./Column";
+import { HuntInfo } from "./HuntInfo";
 
 export type HuntTimelineItemProps = {
   hunt: Hunt;
@@ -41,33 +39,20 @@ export const HuntTimelineItem: React.FC<HuntTimelineItemProps> = ({
     </TimelineSeparator>
     <TimelineContent>
       <PaddedPaper>
-        <ContentRow reverse={reverse} style={{ flex: 1 }}>
-          <Column style={{ flex: 1 }}>
-            <Typography variant="h6" component="h1">
-              {hunt.boss.name}
-            </Typography>
-            <Typography>
-              Every {hunt.boss.spawnCooldown}~
-              {hunt.boss.spawnCooldown + hunt.boss.spawnWindow} minutes on{" "}
-              {hunt.map}
-            </Typography>
-          </Column>
-          <Column>
-            <Tooltip title="Set kill time">
-              <IconButton onClick={() => onEdit(hunt)}>
-                <Edit />
-              </IconButton>
-            </Tooltip>
-          </Column>
-          <Column>
-            <Tooltip title="Stop hunting">
-              <IconButton onClick={() => onDelete(hunt)}>
-                <Delete />
-              </IconButton>
-            </Tooltip>
-          </Column>
-        </ContentRow>
+        <HuntInfo hunt={hunt} />
       </PaddedPaper>
+      <Actions align={reverse ? "right" : "left"}>
+        <Tooltip title="Set kill time">
+          <IconButton onClick={() => onEdit(hunt)}>
+            <Edit />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Stop hunting">
+          <IconButton onClick={() => onDelete(hunt)}>
+            <Delete />
+          </IconButton>
+        </Tooltip>
+      </Actions>
     </TimelineContent>
   </MuiTimelineItem>
 );
@@ -76,7 +61,7 @@ const PaddedPaper = styled(Paper).attrs({ elevation: 3 })`
   padding: 6px 16px;
 `;
 
-const ContentRow = styled(Row)<{ reverse?: boolean }>`
-  flex-direction: ${({ reverse }) => (reverse ? "row-reverse" : "row")};
-  align-items: center;
+const Actions = styled.div<{ align: "left" | "right" }>`
+  text-align: ${({ align }) => align};
+  margin-top: 8px;
 `;
