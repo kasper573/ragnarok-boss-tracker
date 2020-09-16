87f6fd6e1723eeba9c4e3c6af2a933ec2ff65de6
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-16 13:15:59 +0200

    Use built-in dark theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,4 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { default as green } from "@material-ui/core/colors/green";
-import { default as purple } from "@material-ui/core/colors/purple";
-
-const primaryGreen = green[100];
-const accentGreen = green.A200;
-const darkGreen = green[900];
-const primaryPurple = purple[500];
-const accentPurple = purple.A200;
-const darkPurple = purple[900];
 
 export const theme = createMuiTheme({
   overrides: {
@@ -23,18 +14,6 @@ export const theme = createMuiTheme({
     },
   },
   palette: {
-    primary: {
-      light: accentPurple,
-      main: primaryPurple,
-      dark: darkPurple,
-      contrastText: "#fff",
-    },
     type: "dark",
-    secondary: {
-      light: accentGreen,
-      main: primaryGreen,
-      dark: darkGreen,
-      contrastText: "#fff",
-    },
   },
 });
