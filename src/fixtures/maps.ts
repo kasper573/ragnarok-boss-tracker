120e80708de9a552bb198fadd9cada0073188763
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-19 15:10:49 +0200

    Deduplicate maps fixtures

---
+++ src/fixtures/maps.ts
@@ -0,0 +1,268 @@
+export const maps = {
+  moc_pryd06: {
+    name: "moc_pryd06",
+    imageUrl: require("../assets/maps/moc_pryd06.jpg"),
+  },
+  pay_fild04: {
+    name: "pay_fild04",
+    imageUrl: require("../assets/maps/pay_fild04.jpg"),
+  },
+  xmas_dun01: {
+    name: "xmas_dun01",
+    imageUrl: require("../assets/maps/xmas_dun01.jpg"),
+  },
+  yuno_fild03: {
+    name: "yuno_fild03",
+    imageUrl: require("../assets/maps/yuno_fild03.jpg"),
+  },
+  gld2_prt: {
+    name: "gld2_prt",
+    imageUrl: require("../assets/maps/gld2_prt.jpg"),
+  },
+  yuno_fild04: {
+    name: "yuno_fild04",
+    imageUrl: require("../assets/maps/yuno_fild04.jpg"),
+  },
+  gld_dun03_2: {
+    name: "gld_dun03_2",
+    imageUrl: require("../assets/maps/gld_dun03_2.jpg"),
+  },
+  ra_fild03: {
+    name: "ra_fild03",
+    imageUrl: require("../assets/maps/ra_fild03.jpg"),
+  },
+  ra_fild04: {
+    name: "ra_fild04",
+    imageUrl: require("../assets/maps/ra_fild04.jpg"),
+  },
+  ve_fild01: {
+    name: "ve_fild01",
+    imageUrl: require("../assets/maps/ve_fild01.jpg"),
+  },
+  ve_fild02: {
+    name: "ve_fild02",
+    imageUrl: require("../assets/maps/ve_fild02.jpg"),
+  },
+  lou_dun03: {
+    name: "lou_dun03",
+    imageUrl: require("../assets/maps/lou_dun03.jpg"),
+  },
+  gld_dun03: {
+    name: "gld_dun03",
+    imageUrl: require("../assets/maps/gld_dun03.jpg"),
+  },
+  prt_maze03: {
+    name: "prt_maze03",
+    imageUrl: require("../assets/maps/prt_maze03.jpg"),
+  },
+  abbey03: { name: "abbey03", imageUrl: require("../assets/maps/abbey03.jpg") },
+  lhz_dun03: {
+    name: "lhz_dun03",
+    imageUrl: require("../assets/maps/lhz_dun03.jpg"),
+  },
+  lhz_dun04: {
+    name: "lhz_dun04",
+    imageUrl: require("../assets/maps/lhz_dun04.jpg"),
+  },
+  bra_dun02: {
+    name: "bra_dun02",
+    imageUrl: require("../assets/maps/bra_dun02.jpg"),
+  },
+  gld2_gef: {
+    name: "gld2_gef",
+    imageUrl: require("../assets/maps/gld2_gef.jpg"),
+  },
+  gld_dun04_2: {
+    name: "gld_dun04_2",
+    imageUrl: require("../assets/maps/gld_dun04_2.jpg"),
+  },
+  gl_chyard: {
+    name: "gl_chyard",
+    imageUrl: require("../assets/maps/gl_chyard.jpg"),
+  },
+  abyss_03: {
+    name: "abyss_03",
+    imageUrl: require("../assets/maps/abyss_03.jpg"),
+  },
+  gef_dun02: {
+    name: "gef_dun02",
+    imageUrl: require("../assets/maps/gef_dun02.jpg"),
+  },
+  gld_dun04: {
+    name: "gld_dun04",
+    imageUrl: require("../assets/maps/gld_dun04.jpg"),
+  },
+  gef_dun01: {
+    name: "gef_dun01",
+    imageUrl: require("../assets/maps/gef_dun01.jpg"),
+  },
+  treasure02: {
+    name: "treasure02",
+    imageUrl: require("../assets/maps/treasure02.jpg"),
+  },
+  gld_dun01_2: {
+    name: "gld_dun01_2",
+    imageUrl: require("../assets/maps/gld_dun01_2.jpg"),
+  },
+  pay_fild10: {
+    name: "pay_fild10",
+    imageUrl: require("../assets/maps/pay_fild10.jpg"),
+  },
+  lhz_dun02: {
+    name: "lhz_dun02",
+    imageUrl: require("../assets/maps/lhz_dun02.jpg"),
+  },
+  gon_dun03: {
+    name: "gon_dun03",
+    imageUrl: require("../assets/maps/gon_dun03.jpg"),
+  },
+  abbey02: { name: "abbey02", imageUrl: require("../assets/maps/abbey02.jpg") },
+  xmas_fild01: {
+    name: "xmas_fild01",
+    imageUrl: require("../assets/maps/xmas_fild01.jpg"),
+  },
+  gld2_pay: {
+    name: "gld2_pay",
+    imageUrl: require("../assets/maps/gld2_pay.jpg"),
+  },
+  gld2_ald: {
+    name: "gld2_ald",
+    imageUrl: require("../assets/maps/gld2_ald.jpg"),
+  },
+  ra_san05: {
+    name: "ra_san05",
+    imageUrl: require("../assets/maps/ra_san05.jpg"),
+  },
+  prt_sewb4: {
+    name: "prt_sewb4",
+    imageUrl: require("../assets/maps/prt_sewb4.jpg"),
+  },
+  mosk_dun03: {
+    name: "mosk_dun03",
+    imageUrl: require("../assets/maps/mosk_dun03.jpg"),
+  },
+  man_fild03: {
+    name: "man_fild03",
+    imageUrl: require("../assets/maps/man_fild03.jpg"),
+  },
+  thor_v03: {
+    name: "thor_v03",
+    imageUrl: require("../assets/maps/thor_v03.jpg"),
+  },
+  ama_dun03: {
+    name: "ama_dun03",
+    imageUrl: require("../assets/maps/ama_dun03.jpg"),
+  },
+  kh_dun02: {
+    name: "kh_dun02",
+    imageUrl: require("../assets/maps/kh_dun02.jpg"),
+  },
+  iz_dun05: {
+    name: "iz_dun05",
+    imageUrl: require("../assets/maps/iz_dun05.jpg"),
+  },
+  ice_dun03: {
+    name: "ice_dun03",
+    imageUrl: require("../assets/maps/ice_dun03.jpg"),
+  },
+  ayo_dun02: {
+    name: "ayo_dun02",
+    imageUrl: require("../assets/maps/ayo_dun02.jpg"),
+  },
+  dew_dun01: {
+    name: "dew_dun01",
+    imageUrl: require("../assets/maps/dew_dun01.jpg"),
+  },
+  niflheim: {
+    name: "niflheim",
+    imageUrl: require("../assets/maps/niflheim.jpg"),
+  },
+  anthell02: {
+    name: "anthell02",
+    imageUrl: require("../assets/maps/anthell02.jpg"),
+  },
+  gld_dun02_2: {
+    name: "gld_dun02_2",
+    imageUrl: require("../assets/maps/gld_dun02_2.jpg"),
+  },
+  gld_dun02: {
+    name: "gld_dun02",
+    imageUrl: require("../assets/maps/gld_dun02.jpg"),
+  },
+  mjolnir_04: {
+    name: "mjolnir_04",
+    imageUrl: require("../assets/maps/mjolnir_04.jpg"),
+  },
+  gld_dun01: {
+    name: "gld_dun01",
+    imageUrl: require("../assets/maps/gld_dun01.jpg"),
+  },
+  pay_dun04: {
+    name: "pay_dun04",
+    imageUrl: require("../assets/maps/pay_dun04.jpg"),
+  },
+  moc_prydn2: {
+    name: "moc_prydn2",
+    imageUrl: require("../assets/maps/moc_prydn2.jpg"),
+  },
+  gef_fild03: {
+    name: "gef_fild03",
+    imageUrl: require("../assets/maps/gef_fild03.jpg"),
+  },
+  gef_fild10: {
+    name: "gef_fild10",
+    imageUrl: require("../assets/maps/gef_fild10.jpg"),
+  },
+  moc_pryd04: {
+    name: "moc_pryd04",
+    imageUrl: require("../assets/maps/moc_pryd04.jpg"),
+  },
+  in_sphinx5: {
+    name: "in_sphinx5",
+    imageUrl: require("../assets/maps/in_sphinx5.jpg"),
+  },
+  moc_fild17: {
+    name: "moc_fild17",
+    imageUrl: require("../assets/maps/moc_fild17.jpg"),
+  },
+  dic_dun02: {
+    name: "dic_dun02",
+    imageUrl: require("../assets/maps/dic_dun02.jpg"),
+  },
+  ein_dun02: {
+    name: "ein_dun02",
+    imageUrl: require("../assets/maps/ein_dun02.jpg"),
+  },
+  xmas_dun02: {
+    name: "xmas_dun02",
+    imageUrl: require("../assets/maps/xmas_dun02.jpg"),
+  },
+  beach_dun: {
+    name: "beach_dun",
+    imageUrl: require("../assets/maps/beach_dun.jpg"),
+  },
+  spl_fild03: {
+    name: "spl_fild03",
+    imageUrl: require("../assets/maps/spl_fild03.jpg"),
+  },
+  thana_boss: {
+    name: "thana_boss",
+    imageUrl: require("../assets/maps/thana_boss.jpg"),
+  },
+  tur_dun04: {
+    name: "tur_dun04",
+    imageUrl: require("../assets/maps/tur_dun04.jpg"),
+  },
+  odin_tem03: {
+    name: "odin_tem03",
+    imageUrl: require("../assets/maps/odin_tem03.jpg"),
+  },
+  jupe_core: {
+    name: "jupe_core",
+    imageUrl: require("../assets/maps/jupe_core.jpg"),
+  },
+  moc_fild22: {
+    name: "moc_fild22",
+    imageUrl: require("../assets/maps/moc_fild22.jpg"),
+  },
+};
