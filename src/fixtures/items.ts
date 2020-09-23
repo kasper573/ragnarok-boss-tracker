39877966d3e8989d207b69f9c6b67fe83604e7e5
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-24 00:46:16 +0200

    Add ids to items.ts

--- src/fixtures/items.ts
+++ src/fixtures/items.ts
@@ -1,6 +1,12 @@
 // TODO extract all items from:
 // https://github.com/rathena/rathena/blob/master/db/re/item_db.txt
 // https://github.com/rathena/rathena/edit/master/db/re/mob_db.txt
+import { ItemId } from "../state/ItemId";
+
 export const items = {
-  616: { name: "Old Card Album", icon: require("../assets/items/616.gif") },
+  616: {
+    id: 616 as ItemId,
+    name: "Old Card Album",
+    icon: require("../assets/items/616.gif"),
+  },
 };
