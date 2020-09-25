5f5526318b47f568bd1b10eea5daeb995f9aabe9
Kasper Sandin <kasper.sandin@gmail.com>
2020-09-25 04:55:31 +0200

    Generate drops and items code with prettier formatting

--- dev/rAthenaImport/generator/generateDrops.ts
+++ dev/rAthenaImport/generator/generateDrops.ts
@@ -1,11 +1,26 @@
 import * as fs from "fs";
-import { dropsTemplate } from "./dropsTemplate";
 import { ParsedDrop } from "../types/ParsedDrop";
 import { MobId } from "../../../src/state/MobId";
+import { format } from "prettier";
+import { warning } from "./warning";
 
 export const generateDrops = (
   outputFile: string,
   drops: Map<MobId, ParsedDrop[]>
 ) => {
-  fs.writeFileSync(outputFile, dropsTemplate(drops));
+  fs.writeFileSync(
+    outputFile,
+    itemReferences(format(dropsTemplate(drops), { parser: "babel" }))
+  );
 };
+
+const dropsTemplate = (drops: Map<MobId, ParsedDrop[]>) => `// ${warning}.
+import { items } from "./items";
+import { Drop } from "../../state/Drop";
+
+export const drops: Record<string, Drop[]> =
+${JSON.stringify(Object.fromEntries(drops.entries()))};
+`;
+
+const itemReferences = (jsonString: string) =>
+  jsonString.replace(/item: (\d+)/g, (match, id) => `item: items[${id}]`);
--- dev/rAthenaImport/generator/generateItems.ts
+++ dev/rAthenaImport/generator/generateItems.ts
@@ -1,6 +1,21 @@
 import * as fs from "fs";
-import { itemsTemplate } from "./itemsTemplate";
 import { ParsedItem } from "../types/ParsedItem";
+import { format } from "prettier";
+import { warning } from "./warning";
 
 export const generateItems = (outputFile: string, items: ParsedItem[]) =>
-  fs.writeFileSync(outputFile, itemsTemplate(items));
+  fs.writeFileSync(
+    outputFile,
+    addAsItemId(format(itemsTemplate(items), { parser: "babel" }))
+  );
+
+const itemsTemplate = (items: ParsedItem[]) => `// ${warning}.
+import { ItemId } from "../../state/ItemId";
+export const items = ${JSON.stringify(groupById(items))};
+`;
+
+const groupById = (items: ParsedItem[]) =>
+  items.reduce((group, item) => ({ ...group, [item.id]: item }), {});
+
+const addAsItemId = (jsonString: string) =>
+  jsonString.replace(/id: (\d+)/g, (match, id) => `id: ${id} as ItemId`);
